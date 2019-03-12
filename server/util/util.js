e5db1f898e68f483d02693b5cc5fe1ad6b5209b0
Paul Sokolik <paul.sokolik@gmail.com>
2019-03-11 22:22:39 -0700

    remove logs

--- server/util/util.js
+++ server/util/util.js
@@ -6,17 +6,13 @@ const util = {
     // if we can't find the start date ("today's" date),
     // the client sent us bad data.
     if (startIdx === -1) {
-      console.log("Could not find start date");
-      // return res.sendStatus(400);
       return { targetSubstr: null, error: 400 };
     } else if (endIdx === -1 && endDateStr.split(" ")[0] === "Tuesday") {
       // if we can't find the end date string, check if 'tomorrow' is Tuesday (the end of the menu).
       // In this case, set endIdx to the end of the htmlStr string.
       endIdx = htmlStr.length;
     } else if (endIdx === -1) {
-      console.log("Could not find end date");
       // if we still can't find the end date, there's something wrong
-      // return res.sendStatus(400);
       return { targetSubstr: null, error: 400 };
     }
 
@@ -28,7 +24,6 @@ const util = {
     // improperly parse the string
     startIdx = targetSubstr.indexOf('<p') + 3; // opening <p> tag plus its length
     if (startIdx === -1) {
-      // return res.sendStatus(500);
       return { targetSubstr: null, error: 500 };
     }
     targetSubstr = targetSubstr.substring(startIdx).trim();
@@ -36,7 +31,6 @@ const util = {
     // end <p> tag
     endIdx = targetSubstr.indexOf('</p>');
     if (endIdx === -1) {
-      // return res.sendStatus(500);
       return { targetSubstr: null, error: 500 };
     }
     targetSubstr = targetSubstr.substring(0, endIdx).trim();
@@ -44,7 +38,6 @@ const util = {
     // there should just be one closing tag left at the beginning now
     startIdx = targetSubstr.indexOf('>') + 1;
     if (startIdx === -1) {
-      // return res.sendStatus(500);
       return { targetSubstr: null, error: 500 };
     }
     targetSubstr = targetSubstr.substring(startIdx).trim();
